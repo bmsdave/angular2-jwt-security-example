bc1d5ecfecc31ecab044584766a4631addabbdb1
CLTanuki <cltanuki@gmail.com>
2016-02-29 14:25:34 +0300

    Added: Activation component files.

---
+++ src/app/auth/components/activation/activation-component.ts
@@ -0,0 +1,90 @@
+import {Component} from 'angular2/core';
+import {Router} from 'angular2/router';
+import {FORM_DIRECTIVES, Validators} from 'angular2/common';
+import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
+import {AuthService} from '../../auth-service';
+import { IUser } from '../../../base/interfaces/interfaces';
+
+@Component({
+  selector: 'login',
+  template: require('./login.html'),
+  providers: [AuthService],
+  directives: [MATERIAL_DIRECTIVES, FORM_DIRECTIVES]
+})
+
+export class Activation {
+
+  user: IUser;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) {
+    this.authService = authService;
+    this.user = {
+      id: null,
+      username: null,
+      person:
+      {
+        user: null,
+        first_name: null,
+        last_name: null,
+        mid_name: null,
+        date_of_birth: null,
+        sex: null,
+        bio: null,
+        emails:
+        [
+          { id: null, cat: null, body: null }
+        ],
+        positions:
+        [
+          {
+            id: null,
+            unit:
+            {
+              id: null,
+              title: null,
+              parent: null,
+              corp:
+              {
+                id: null,
+                title: null
+              }
+            },
+            title: null,
+            since: null,
+            until: null
+          }
+        ],
+        phones:
+        [
+          {
+            id: null,
+            cat: null,
+            country_code: null,
+            area_code: null,
+            number: null
+          }
+        ]
+      }
+    };
+  }
+
+  activate() {
+    console.log('inside login.ts function');
+
+    this.authService.login(this.user)
+      .subscribe(
+      res => {
+        this.authService.saveJwt(res.token);
+      },
+      err => {
+      },
+      () => {
+        this.authService.token = localStorage.getItem('token');
+        this.router.navigate(['Base']);
+      }
+    );
+  }
+}
