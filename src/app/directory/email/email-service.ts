fad19c7aedaa2a598d60c0a386b29523a5fb18fb
CLTanuki <cltanuki@gmail.com>
2016-03-06 22:34:03 +0300

    Updated: get and fetch methods.

--- src/app/directory/email/email-service.ts
+++ src/app/directory/email/email-service.ts
@@ -5,31 +5,33 @@ import {Http, Headers, Response} from 'angular2/http';
 import {Router} from 'angular2/router';
 import {EMail} from '../../base/classes/email';
 
-
-// TODO: refactor setDefaultTask
-
 @Injectable()
-export class EmailService {
+export class EMailService {
 
   public baseUrl = '/';
-  public email: Observable<EMail>;
+  public emails: Observable<EMail>;
+  public selectedEMail: Observable<EMail>;
 
-  private _emailObserver: any;
-  private _email = EMail;
+  private _emailsObserver: any;
+  private _emails: EMail[] = [];
+  private _selectedEMailObserver: any;
 
   constructor(
     private http: Http,
     private router: Router,
     private authHttp: AuthHttp
   ) {
-    this.retrieveEmails();
+    this.getEMails();
 
-    this.email = new Observable(observer =>
-      this._email = observer);
+    this.selectedEMail = new Observable(observer =>
+      this._selectedEMailObserver = observer);
+
+    this.emails = new Observable(observer =>
+      this._emailsObserver = observer);
   }
 
-  retrieveEmails() {
-    console.log('Retrieve Email');
+  getEMails() {
+    console.log('getEMails');
 
     var header = new Headers();
     header.append('Content-Type', 'application/json');
@@ -40,11 +42,21 @@ export class EmailService {
       .map(res => res.json()).subscribe(
         data => {
           console.log(data);
-          this._email= new EMail(data);
+          for ( var item in data ) {
+            this._emails.push(new EMail(item));
+          }
         },
-        err => console.log('getEmail.error: ', err),
-        () => console.log('get email complete')
+        err => console.log('getUser.error: ', err),
+        () => console.log('get user complete')
       );
   };
+
+  fetchEMails() {
+    this._emailsObserver.next(this._emails);
+  }
+
+  selectEmail(email) {
+    this._selectedEMailObserver.next(email);
+  }
 
 }
