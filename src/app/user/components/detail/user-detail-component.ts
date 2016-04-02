106553deaea104b12c02471db357d9798d3dac8e
Vadim Gorbachev <v.gorbachev@i-free.com>
2016-04-03 01:02:35 +0300

    fetch user-detail

--- src/app/user/components/detail/user-detail-component.ts
+++ src/app/user/components/detail/user-detail-component.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { UserService } from '../../user-service';
 import { User } from '../../../user/user';
 import { CORE_DIRECTIVES } from 'angular2/common';
-import { Router, RouterLink } from 'angular2/router';
+import { Router, RouterLink, RouteParams } from 'angular2/router';
 
 @Component({
     selector: 'user-detail',
@@ -16,13 +16,17 @@ export class UserDetail {
   title:string = 'User Detail';
   private user: User = new User({id: null, username: null, is_auth: false});
 
-  constructor(private UserService:UserService) {
+  constructor(
+    private UserService:UserService,
+    private _routeParams:RouteParams
+  ) {
     UserService.selectedUser$.subscribe( user => this.user = new User(user));
   }
     
   ngOnInit() {
+      let username = this._routeParams.get('username');
       this.UserService.selectedUser$.subscribe( user => this.user = new User(user));
-      this.UserService.next();
+      this.UserService.getSelectedUser(username);
   }
     
 }
