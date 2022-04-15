046867f195c3b55c0af58c51e75304ad4708cfdb
Vadim Gorbachev <bmsdave@gmail.com>
2022-04-15 10:30:03 +0100

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,19 +1,12 @@
-// @AngularClass
-
 exports.config = {
   baseUrl: 'http://localhost:3000/',
-
-  // use `npm run e2e`
   specs: [
     'src/**/**.e2e.ts',
     'src/**/*.e2e.ts'
   ],
   exclude: [],
-
   framework: 'jasmine',
-
   allScriptsTimeout: 110000,
-
   jasmineNodeOpts: {
     showTiming: true,
     showColors: true,
@@ -22,26 +15,16 @@ exports.config = {
     defaultTimeoutInterval: 400000
   },
   directConnect: true,
-
   capabilities: {
     'browserName': 'chrome',
     'chromeOptions': {
       'args': ['show-fps-counter=true']
     }
   },
-
   onPrepare: function() {
     browser.ignoreSynchronization = true;
   },
 
   seleniumServerJar: "node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar",
-
-  /**
-   * Angular 2 configuration
-   *
-   * useAllAngular2AppRoots: tells Protractor to wait for any angular2 apps on the page instead of just the one matching
-   * `rootEl`
-   *
-   */
    useAllAngular2AppRoots: true
 };
